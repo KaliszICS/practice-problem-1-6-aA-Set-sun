bf9694e68fa285c4c836236f79d5778f25175109
aA-Set-sun <343303426@tdsb.ca>
2026-02-20 16:38:45 +0000

    ee

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -30,19 +30,42 @@ public class PracticeProblem {
 		System.out.print("Input a boolean: ");
 		bool = input.nextBoolean();
 		System.out.println(bool);
-		
+		input.nextLine(); //clear scanner
 
-	}
+		}
 
 	public static void q2() {
 		//Write question 2 code here
 		//Ask the user to "Input an integer: ". Save the integer in a variable. Output true when the variable is greater than 5. false otherwise.
+		
+		Scanner input = new Scanner(System.in);
+		int num;
+
+		System.out.print("Input an integer: ");
+		num = input.nextInt();
+		System.out.println(num > 5);
+		
+		input.nextLine(); //clear scanner
+
+
 	}
+
 
 	public static void q3() {
 		//Write question 3 code here
 		//Ask the user to "Input pizza: ". Save the word in a variable. 
 		// Output true when the variable is pizza. false otherwise.
+
+		Scanner input = new Scanner(System.in);
+		String pizza;
+
+		System.out.print("Input pizza: ");
+		pizza = input.nextLine();
+		System.out.println(pizza.equals("pizza"));
+		
+		input.nextLine(); //clear scanner
+
+
 	}
 
 	public static void q4() {
@@ -51,17 +74,50 @@ public class PracticeProblem {
 		Save the number in a variable. 
 		Output true when the number is equal to 0. false otherwise.
 		*/
+
+		Scanner input = new Scanner(System.in);
+		
+		int num;
+		System.out.print("Input a number: ");
+		num = input.nextInt();
+		System.out.println(num == 0);
+		
+		input.nextLine(); //clear scanner
+	
+
 	}
 
 	public static void q5() {
 		//Write question 5 code here
 		/*Ask the user to "Input an integer: " Save the integer in a variable. 
 		Output true when the variable is not 0. false Otherwise */
+
+		Scanner input = new Scanner(System.in);
+		int integer;
+
+		System.out.print("Input an integer: ");
+		integer = input.nextInt();
+		
+		System.out.print(integer != 0);
+		System.out.print("\n");
+		
+		input.nextLine();
+
 	}
 
 	public static void q6() {
 	/*Ask the user to "Input a word earlier than google: " Save the word in a variable. 
 	Output true when the variable is earlier in the dictionary than google. false otherwise. */
+
+	Scanner input = new Scanner(System.in);
+
+	String oldWord;
+	System.out.println("Input a word earlier than google: ");
+	oldWord = input.nextLine();
+
+	System.out.print(oldWord.compareTo( "Google") < 0);
+	input.nextLine();
+
 
 	}
 
